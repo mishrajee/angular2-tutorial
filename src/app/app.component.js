3dbb5f691d171e8d364877726e24defd0136fbeb
Abhinava Mishra <abhinava.mis@gmail.com>
2017-02-14 16:43:59 +0530

    added list of villains, master detail pattern to show detail of selected villain below master list

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -15,18 +15,30 @@ var Villain = (function () {
     return Villain;
 }());
 exports.Villain = Villain;
+var VILLAINS = [
+    { id: 1, name: 'Skeletor' },
+    { id: 2, name: 'Don Karnage' },
+    { id: 3, name: 'Beagle Boys' },
+    { id: 4, name: 'Bane' },
+    { id: 5, name: 'Joker' },
+    { id: 6, name: 'Doctor Jackal' },
+    { id: 7, name: 'Kapala' },
+    { id: 9, name: 'Kilvish' },
+    { id: 10, name: 'Aniken Skywalker' }
+];
 var AppComponent = (function () {
     function AppComponent() {
         this.title = 'Tour of Villains';
-        this.villain = {
-            id: 1,
-            name: 'Skeletor'
-        };
+        this.villains = VILLAINS;
     }
+    AppComponent.prototype.onSelect = function (villain) {
+        this.selectedVillain = villain;
+    };
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "\n  <h1>{{title}}</h1>\n  <h2>{{villain.name}} details!</h2>\n  <div><label>id: </label>{{villain.id}}</div>\n  <div>\n      <label>name: </label>\n      <input [(ngModel)]=\"villain.name\" placeholder=\"name\">\n  </div>\n  "
+            styles: ["\n  .selected {\n    background-color: #CFD8DC !important;\n    color: white;\n  }\n  .villains {\n    margin: 0 0 2em 0;\n    list-style-type: none;\n    padding: 0;\n    width: 15em;\n  }\n  .villains li {\n    cursor: pointer;\n    position: relative;\n    left: 0;\n    background-color: #EEE;\n    margin: .5em;\n    padding: .3em 0;\n    height: 1.6em;\n    border-radius: 4px;\n  }\n  .villains li.selected:hover {\n    background-color: #BBD8DC !important;\n    color: white;\n  }\n  .villains li:hover {\n    color: #607D8B;\n    background-color: #DDD;\n    left: .1em;\n  }\n  .villains .text {\n    position: relative;\n    top: -3px;\n  }\n  .villains .badge {\n    display: inline-block;\n    font-size: small;\n    color: white;\n    padding: 0.8em 0.7em 0 0.7em;\n    background-color: #607D8B;\n    line-height: 1em;\n    position: relative;\n    left: -1px;\n    top: -4px;\n    height: 1.8em;\n    margin-right: .8em;\n    border-radius: 4px 0 0 4px;\n  }\n"],
+            template: "\n  <h1>{{title}}</h1>\n  <h2>My Villains</h2>\n  <ul class=\"villains\">\n    <li *ngFor=\"let villain of villains\"\n      (click)=\"onSelect(villain)\"\n      [class.selected]=\"villain === selectedVillain\"\n      >\n      <span class=\"badge\">{{villain.id}}</span> {{villain.name}}\n    </li>\n  </ul>\n\n  <div *ngIf=\"selectedVillain\">\n    <h2>{{selectedVillain.name}} details!</h2>\n    <div><label>id: </label>{{selectedVillain.id}}</div>\n    <div>\n        <label>name: </label>\n        <input [(ngModel)]=\"selectedVillain.name\" placeholder=\"name\"/>\n    </div>\n  </div>\n\n  "
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,23 +5,99 @@ export class Villain {
   name: string;
 }
 
+const VILLAINS: Villain[] = [
+  {id: 1, name: 'Skeletor'},
+  {id: 2, name: 'Don Karnage'},
+  {id: 3, name: 'Beagle Boys'},
+  {id: 4, name: 'Bane'},
+  {id: 5, name: 'Joker'},
+  {id: 6, name: 'Doctor Jackal'},
+  {id: 7, name: 'Kapala'},
+  {id: 9, name: 'Kilvish'},
+  {id: 10, name: 'Aniken Skywalker'}
+];
+
 @Component({
   selector: 'my-app',
+  styles: [`
+  .selected {
+    background-color: #CFD8DC !important;
+    color: white;
+  }
+  .villains {
+    margin: 0 0 2em 0;
+    list-style-type: none;
+    padding: 0;
+    width: 15em;
+  }
+  .villains li {
+    cursor: pointer;
+    position: relative;
+    left: 0;
+    background-color: #EEE;
+    margin: .5em;
+    padding: .3em 0;
+    height: 1.6em;
+    border-radius: 4px;
+  }
+  .villains li.selected:hover {
+    background-color: #BBD8DC !important;
+    color: white;
+  }
+  .villains li:hover {
+    color: #607D8B;
+    background-color: #DDD;
+    left: .1em;
+  }
+  .villains .text {
+    position: relative;
+    top: -3px;
+  }
+  .villains .badge {
+    display: inline-block;
+    font-size: small;
+    color: white;
+    padding: 0.8em 0.7em 0 0.7em;
+    background-color: #607D8B;
+    line-height: 1em;
+    position: relative;
+    left: -1px;
+    top: -4px;
+    height: 1.8em;
+    margin-right: .8em;
+    border-radius: 4px 0 0 4px;
+  }
+`],
   template: `
   <h1>{{title}}</h1>
-  <h2>{{villain.name}} details!</h2>
-  <div><label>id: </label>{{villain.id}}</div>
-  <div>
-      <label>name: </label>
-      <input [(ngModel)]="villain.name" placeholder="name">
+  <h2>My Villains</h2>
+  <ul class="villains">
+    <li *ngFor="let villain of villains"
+      (click)="onSelect(villain)"
+      [class.selected]="villain === selectedVillain"
+      >
+      <span class="badge">{{villain.id}}</span> {{villain.name}}
+    </li>
+  </ul>
+
+  <div *ngIf="selectedVillain">
+    <h2>{{selectedVillain.name}} details!</h2>
+    <div><label>id: </label>{{selectedVillain.id}}</div>
+    <div>
+        <label>name: </label>
+        <input [(ngModel)]="selectedVillain.name" placeholder="name"/>
+    </div>
   </div>
+
   `
 })
 
 export class AppComponent  {
   title = 'Tour of Villains';
-  villain: Villain = {
-    id: 1,
-    name: 'Skeletor'
-  };
+  villains = VILLAINS;
+  selectedVillain: Villain;
+  
+  onSelect(villain: Villain): void{
+    this.selectedVillain = villain;
+  }
 }
